14d265c4ad8d6e57416a73ac9c40c7ef7bc88a39
Ben Pringle <benjamin.m.pringle@gmail.com>
2020-08-08 17:14:27 -0400

    version update

--- src/main/java/net/whatamidoingstudios/lacroix/LaCroix.java
+++ src/main/java/net/whatamidoingstudios/lacroix/LaCroix.java
@@ -77,7 +77,7 @@ import net.whatamidoingstudios.lacroix.network.LaCroixNetworkHandler;
 @Mod(modid = LaCroix.MODID, version = LaCroix.VERSION)
 public class LaCroix {
 	public static final String MODID = "lacroix";
-	public static final String VERSION = "1.7.0";
+	public static final String VERSION = "1.8.0";
 	
 	@SidedProxy(clientSide = "net.whatamidoingstudios.lacroix.ClientProxyLaCroix", serverSide = "net.whatamidoingstudios.lacroix.ServerProxyLaCroix")
 	public static IProxyLaCroix proxy;
